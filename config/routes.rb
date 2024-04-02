b7fe46ee9e08c3ba0ec5294353c32901b019bf5f
pradhumnayubi <pradhumnasingh.rajput@go-yubi.com>
2024-04-02 16:53:03 +0530

    Finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  root 'application#hello'
+
 
   # Defines the root path route ("/")
   # root "posts#index"
